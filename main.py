8266989ca32410402872dd6fae26985e8e37bd33
khaleoghliFan <fanavakhaleoghli@gmail.com>
2025-04-03 00:48:12 +0330

    access via all url (postman)

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware #برای دسترسی به postman
 
 app = FastAPI()
-
+# تنظیمات CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # اینجا می‌توانید فقط دامنه‌های خاص را مشخص کنید
+    allow_credentials=True,
+    allow_methods=["*"],  # به همه متدها دسترسی بدهید
+    allow_headers=["*"],  # به همه هدرها دسترسی بدهید
+)
 
 @app.get("/")
 async def root():
